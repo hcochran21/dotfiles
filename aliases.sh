715cfcf631afb1d9cee0b5122bda870a0a193aeb
hcochran-gloo <96544991+hcochran-gloo@users.noreply.github.com>
2022-01-05 16:11:22 -0700

    Add git aliases

---
+++ aliases.sh
@@ -0,0 +1,34 @@
+# misc
+alias la='ls -lah'
+# output my ip address
+alias ip='ipconfig getifaddr en0'
+# output my ip address and copy to clipboard (mac only)
+alias cip='ipconfig getifaddr en0 | pbcopy'
+# remove any rogue swap files
+alias killswaps='find . -iname "*.sw[p|n|m]" | xargs rm'
+# source zshrc
+alias szrc="source ~/.zshrc"
+
+# git
+alias gst='git status'
+alias gsl='git stash list'
+alias gss='git stash show -p'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gl='git log'
+alias glo='git log --oneline'
+alias gb='git branch'
+alias gbv='git branch -vv'
+alias gbmm='git branch --merged master'
+alias gbms='git branch --merged staging'
+alias gbmd='git branch --merged development'
+alias gcom='git checkout master'
+alias gcos='git checkout staging'
+alias gcod='git checkout development'
+alias gp='git pull'
+alias gf='git fetch --prune'
+alias ga='git add'
+alias gaa='git add .'
+alias gaac='git add . && git commit'
+alias gpoh='git push origin HEAD'
+alias gpuoh='git push -u origin HEAD'
